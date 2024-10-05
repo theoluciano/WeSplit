24795a2f37c67e880a894cd59063d7fb5b55ee89
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-10-05 13:59:42 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Theo Luciano on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
